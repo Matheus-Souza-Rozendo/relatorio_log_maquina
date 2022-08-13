14c3b949f216af456b52ad90d6e8c4f216edd634
Matheus Souza Rozendo <matheusrozendo234674@gmail.com>
2022-08-13 15:20:31 -0300

    Update log.cpp

--- log.cpp
+++ log.cpp
@@ -1,18 +1,20 @@
 #include <bits/stdc++.h>
-#define numero_de_materias_primas 3
-#define numero_de_status 4
-using namespace std;
 #include "classes/Arquivo_TXT_de_Entrada_log.h"
 #include "classes/Arquivo_TXT_de_Saida_log.h"
 #include "classes/Tabela_log.h"
 #include "classes/Evento.h"
 #include "classes/leitura_dados.h"
 
+#define numero_de_materias_primas 3
+#define numero_de_status 4
+
+using namespace std;
+
 void gerar_relatorio(Arquivo_TXT_de_Entrada_log E){
     Arquivo_TXT_de_Saida_log S;
     if(S.Set_arquivo_saida_log("relatorio","relatorios")){
         S.gerar_relatorio(E.Get_eventos());
-        cout << "Relatório Gerado com Sucesso\n";
+        cout << "RelatÃ³rio Gerado com Sucesso\n";
     }else{
         cout << "Falha ao criar o Arquivo\n";
     }
@@ -21,68 +23,50 @@ void gerar_relatorio(Arquivo_TXT_de_Entrada_log E){
 
 void mostrar_informacoes(Arquivo_TXT_de_Entrada_log E){
     cout << fixed << setprecision(2);
-    string nomes_operacao[7]={"","carregar matéria-prima 1","carregar matéria-prima 2","carregar matéria-prima 3","processo de secagem","processo de lavagem","processo de tingimento"};
-    string nome_status[4]={"operação bem sucedida","sobrecarga leve","sobrecarga com probabilidade de dano","parada por erro"};
+    string nomes_operacao[7]={"","carregar matÃ©ria-prima 1","carregar matÃ©ria-prima 2","carregar matÃ©ria-prima 3","processo de secagem","processo de lavagem","processo de tingimento"};
+    string nome_status[4]={"operaÃ§Ã£o bem sucedida","sobrecarga leve","sobrecarga com probabilidade de dano","parada por erro"};
     Tabela_log tabela;
     tabela.Set_tabela_log(E.Get_eventos());
     vector <Evento> maiores;
     maiores = tabela.operacoes_sem_erro();
-
-
-    cout<< "\n\n --------------------------Maiores operações sem terminar com erro ------------------------------------------\n";
+    cout<< "\n\n --------------------------Maiores operaÃ§Ãµes sem terminar com erro ------------------------------------------\n";
     for(int i=0;i<maiores.size();i++){
-        cout << "OPERAÇÃO " << i+1 << endl;
+        cout << "OPERAÃ‡ÃƒO " << i+1 << endl;
         cout << "Data: Dia "<< maiores[i].Get_dia() << "   as  " << maiores[i].Get_hora() << ":" << maiores[i].Get_minuto() << endl;
-        cout << "Operação: " << nomes_operacao[maiores[i].Get_operacao()] << endl;
-        cout << "Duração: " << maiores[i].Get_duracao() << " min\n";
+        cout << "OperaÃ§Ã£o: " << nomes_operacao[maiores[i].Get_operacao()] << endl;
+        cout << "DuraÃ§Ã£o: " << maiores[i].Get_duracao() << " min\n";
         cout << "Status: " << nome_status[maiores[i].Get_status()] << endl << endl;
     }
-
-
-    cout <<"\n\n --------------------- Total de Duração de Cada Status ---------------------------------------------------\n";
+    cout <<"\n\n --------------------- Total de DuraÃ§Ã£o de Cada Status ---------------------------------------------------\n";
     for(int i=0;i<numero_de_status;i++){
         cout << nome_status[i] << " : " << tabela.duracao_por_status(i) <<" min" << endl;
     }
-
-
-    cout <<"\n\n --------------------- Percentual de Cada Matéria Prima Carregada na Maquina ------------------------------\n";
+    cout <<"\n\n --------------------- Percentual de Cada MatÃ©ria Prima Carregada na Maquina ------------------------------\n";
     for(int i=1;i<=numero_de_materias_primas;i++){
         float percentual = tabela.numero_carregamento_materia_prima(i)/float(tabela.numero_carregamento_materia_prima(1)+tabela.numero_carregamento_materia_prima(2)+tabela.numero_carregamento_materia_prima(3));
-        cout << "Matéria-Prima " << i << " : " << percentual*100 << "%" << endl;
+        cout << "MatÃ©ria-Prima " << i << " : " << percentual*100 << "%" << endl;
     }
-
-
-    cout <<"\n\n --------------------- Percentual de Operações Bem-sucedidas em relação as demais ------------------------------\n";
+    cout <<"\n\n --------------------- Percentual de OperaÃ§Ãµes Bem-sucedidas em relaÃ§Ã£o as demais ------------------------------\n";
     float percentual = tabela.numero_operacao_por_status(0)/float(tabela.numero_operacao_por_status(0)+tabela.numero_operacao_por_status(1)+tabela.numero_operacao_por_status(2)+tabela.numero_operacao_por_status(3));
     cout << percentual*100 << '%' << endl;
-
-    cout <<"\n\n --------------------- Percentual de operações Bem-sucedidas em relação as mal-sucedidas ------------------------------\n";
+    cout <<"\n\n --------------------- Percentual de operaÃ§Ãµes Bem-sucedidas em relaÃ§Ã£o as mal-sucedidas ------------------------------\n";
     percentual=tabela.numero_operacao_por_status(0)/tabela.numero_operacao_por_status(3);
     percentual=percentual*100;
     cout<<percentual << '%' << endl;
-
-
     cout << "\n\n --------------------------------------------------------------------------------------------------------------------\n";
-    cout << "a máquina foi carregada com a matéria-prima 1 no período da manhã: " << tabela.numero_carregamentos__materia_prima_manha(1) << " vezes\n";
-
-
-    cout <<"\n\n ---------------------- os dois status de menor duração desconsiderando-se os erros -----------------------------------\n";
-    float **matriz = cria_matriz(3,2);
+    cout << "a mÃ¡quina foi carregada com a matÃ©ria-prima 1 no perÃ­odo da manhÃ£: " << tabela.numero_carregamentos__materia_prima_manha(1) << " vezes\n";
+    cout <<"\n\n ---------------------- os dois status de menor duraÃ§Ã£o desconsiderando-se os erros -----------------------------------\n";
+    vector <vector<int>> matriz = cria_matriz(3,2);
     for(int i=0;i<numero_de_status-1;i++){
         matriz[i][0]=i;
         matriz[i][1]=tabela.duracao_por_status(i);
     }
-    matriz=ordenar_matriz_ordem_crescente(matriz,1,3);
-    int indice=matriz[0][0];
-    int minutos=matriz[0][1];
-    cout << nome_status[indice] << " : " <<  minutos <<" min" << endl;
-    indice=matriz[1][0];
-    minutos=matriz[1][1];
-    cout << nome_status[indice] << " : " <<  minutos <<" min" << endl;
-
-
+    matriz=ordenar_matriz_ordem_crescente<vector<vector<int>>>(matriz,1);
+    for(int i=0;i<2;i++){
+        cout << nome_status[matriz[i][0] << " : " <<  matriz[i][1] <<" min" << endl;
+    }
     cout <<"\n\n ------------------------------------------------------------------------------------------------\n";
-    cout << "Número de Erros com mais de 5 minutos: " << tabela.numero_erro_maior_5_minutos() << endl;
+    cout << "NÃºmero de Erros com mais de 5 minutos: " << tabela.numero_erro_maior_5_minutos() << endl;
 }
 
 int main(){
@@ -97,9 +81,9 @@ int main(){
     if(E.Set_arquivo_log(caminho)){
         inicio:
         system("cls");
-        cout << "Digite 1 para Gerar um Relatório\n";
-        cout << "Digite 2 para Mostrar Informações\n";
-        cout <<"opção: ";
+        cout << "Digite 1 para Gerar um RelatÃ³rio\n";
+        cout << "Digite 2 para Mostrar InformaÃ§Ãµes\n";
+        cout <<"opÃ§Ã£o: ";
         cin >> op;
         if(op=="1"){
             gerar_relatorio(E);
@@ -111,14 +95,14 @@ int main(){
                 cout << endl << endl;
                 system("pause");
             }else{
-                cout << "Opção Invalida" << endl;
+                cout << "OpÃ§Ã£o Invalida" << endl;
                 system("pause");
             }
         }
         cout << "\n\n\nDigite 1 para Sair\n";
         cout <<"Digite 2 para trocar o arquivo de log\n";
         cout << "Digite qualquer outra tecla para voltar ao menu principal\n";
-        cout <<"opção: ";
+        cout <<"opÃ§Ã£o: ";
         cin >> op;
         if(op=="2"){
             goto troca_caminho;
@@ -128,7 +112,7 @@ int main(){
             }
         }
     }else{
-        cout << "Arquivo não encontrado\n";
+        cout << "Arquivo nÃ£o encontrado\n";
         cout << "\n\n\nDigite 1 para Sair\nDigite qualquer tecla para continuar\nop: ";
         cin >> op;
         if(op!="1"){
